525f774752369e0afc1a3e02c0c44a416dfea79c
pabloalvare <pabloalvarez.pl@gmail.com>
2022-12-09 20:37:41 -0300

    header working

--- queChurro_react/quechurro/src/App.js
+++ queChurro_react/quechurro/src/App.js
@@ -1,17 +1,19 @@
 import Header from './Componets/Header/Header';
 import './App.css';
-import FirstSection from './Componets/Content/FirstSection';
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/vendor/aos/aos.css'
+import './assets/vendor/glightbox/css/glightbox.min.css'
+import './assets/vendor/swiper/swiper-bundle.min.css'
+import './assets/css/main.css'
+import './assets/css/blobz.css'
+import './assets/css/blobz.min.css'
+import './assets/css/blobz-screen-sizes.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <br/>
-      <div className="justify-content-flex-start">
-        <div className="col-sm">
-          <FirstSection/>
-        </div>
-      </div>
     </div>
   );
 }
--- queChurro_react/quechurro/src/Componets/Header/Header.js
+++ queChurro_react/quechurro/src/Componets/Header/Header.js
@@ -3,7 +3,31 @@ import NavBar from './NavBar'
 
 function Header() {
   return (
-     <NavBar/>
+    <header id="header" className="header fixed-top d-flex align-items-center">
+        <div className="container d-flex align-items-center justify-content-between">
+
+          <div className="logo d-flex align-items-center me-auto me-lg-0">
+            <h1>QuéChurro<span>.</span></h1>
+          </div>
+
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li><a>Inicio</a></li>
+              <li><a>Acerca de QuéChurro</a></li>
+              <li><a>Menu</a></li>
+              <li><a>Eventos</a></li>
+              <li><a>Reservas</a></li>
+              <li><a>Galería</a></li>
+              <li><a>Contacto</a></li>
+            </ul>
+          </nav>
+
+          <a className="btn-book-a-table" href="#book-a-table">Reservá tu evento</a>
+          <i className="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+          <i className="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+        </div>
+      </header>
   )
 }
 
